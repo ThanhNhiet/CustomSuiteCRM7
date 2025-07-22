e529d0e3055ac39ab1019915c5c49d8d56f685b5
PhamLeThanhNhiet <nhiethiz@gmail.com>
2025-07-22 14:01:11 +0700

    Add Push Notification functionality with new PushTokenController and hooks for Meetings, Notes, and Tasks

--- application/Ext/Api/V8/controllers.php
+++ application/Ext/Api/V8/controllers.php
@@ -2,10 +2,12 @@
 require_once 'custom/application/Ext/Api/V8/Controller/UserPasswordController.php';
 require_once 'custom/application/Ext/Api/V8/Controller/ListViewController.php';
 require_once 'custom/application/Ext/Api/V8/Controller/LanguageModuleController.php';
+require_once 'custom/application/Ext/Api/V8/Controller/PushTokenController.php';
 
 use Api\V8\Controller\UserPasswordController;
 use Api\V8\Controller\ListViewController;
 use Api\V8\Controller\LanguageModuleController;
+use Api\V8\Controller\PushTokenController;
 use Slim\Container;
 
 return [
@@ -17,5 +19,8 @@ return [
     },
     'LanguageModuleController' => function (Container $container) {
         return new LanguageModuleController();
-    }
+    },
+    'PushTokenController' => function (Container $container) {
+        return new PushTokenController();
+    },
 ];
