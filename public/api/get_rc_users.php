9e36b01319b8a756607a85b4cccfb2a75ee257a6
PhamLeThanhNhiet <nhiethiz@gmail.com>
2026-02-09 16:57:58 +0700

    feat: enhance user presence API response by filtering out bot users

--- public/api/get_rc_users.php
+++ public/api/get_rc_users.php
@@ -25,7 +25,26 @@ header('Content-Type: application/json');
 if ($err || $http_code !== 200) {
     echo json_encode(['success' => false, 'error' => 'Server Error']);
 } else {
-    echo $response;
+    $data = json_decode($response, true);
+    
+    if ($data && isset($data['users'])) {
+        $excluded_bots = [
+            'jitsi.bot',
+            'bigbluebutton.bot', 
+            'create-task-for-suitecrm.bot'
+        ];
+        
+        $filtered_users = array_filter($data['users'], function($user) use ($excluded_bots) {
+            $username = isset($user['username']) ? $user['username'] : '';
+            return !in_array($username, $excluded_bots);
+        });
+        
+        $data['users'] = array_values($filtered_users);
+        
+        echo json_encode($data);
+    } else {
+        echo $response;
+    }
 }
 exit;
 ?>
