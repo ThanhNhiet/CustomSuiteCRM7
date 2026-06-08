7321deeea3e2abb149fc798edecf8de477cf83da
PhamLeThanhNhiet <nhiethiz@gmail.com>
2026-06-08 12:30:38 +0700

    feat: add language string definitions and module installer package

--- include/language/en_us.lang.php
+++ include/language/en_us.lang.php
@@ -4,4 +4,12 @@ $app_list_strings['vessel_crew_list_only']=array (
   'sgt_file_module' => 'File Module',
 );
 $app_strings['LBL_TABGROUP_SALES'] = 'Test';
-?>
+
+$app_list_strings['discount_type_list']=array (
+  'amt' => 'amount',
+  'pct' => 'Percent (%)',
+);
+$app_list_strings['discount_type_list']=array (
+  'amt' => 'Amount',
+  'pct' => 'Percent (%)',
+);
